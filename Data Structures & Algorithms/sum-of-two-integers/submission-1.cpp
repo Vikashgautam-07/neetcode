498ac99f870356906cc7f7fc5ac5ced24e80ec73
Vikash Gautam <105532611+Vikashgautam-07@users.noreply.github.com>
2026-06-09 15:56:21 +0530

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int carry = 0;
+        
+        while ( b)
+        {
+            carry = a & b;
+
+            a = a ^ b;
+
+            b = (unsigned int) carry << 1;
+        }
+
+        return a;
+    }
+};
